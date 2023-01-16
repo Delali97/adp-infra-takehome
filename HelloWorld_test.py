aea5859b8dea7764a9d8ad6bc378ad9ebbd0da43
Delali97 <jennifer.kpolu@yahoo.com>
2023-01-16 19:54:59 -0400

    Adding Test File

---
+++ HelloWorld_test.py
@@ -0,0 +1,35 @@
+import pytest
+from flask.testing import FlaskClient
+from app import app
+@pytest.fixture(scope='module')
+
+
+
+def client():
+    return app.test_client()
+
+
+def test_get_home(client: FlaskClient):
+    """should be a successful GET request"""
+    resp = client.get('/')
+    assert resp.status_code == 200
+    
+
+def test_post_home(client: FlaskClient):
+    """should be a successful POST request"""
+    resp = client.post('/')
+    assert resp.status_code == 200
+    assert resp.json["message"] == "Empty POST request"
+
+def test_no_accept_header(client: FlaskClient):
+    """a request with header.accept not json/application should return a byte 'Hello, World"""
+    resp = client.get('/')
+    assert "Hello, World" in resp.data
+    
+
+
+def test_accept_header_json_application(client: FlaskClient):
+    """a request with header.accept = json/application should return a json object {message: Hello World"""
+    resp = client.get('/', headers={"Accept": "application/json"})
+    assert resp.status_code == 200
+    assert resp.json["message"] == "Hello, World"
